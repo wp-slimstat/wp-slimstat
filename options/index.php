09a5826718f58eda7a4045c0b4e4932f0cb42d43
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-19 19:31:15 +0000

    Archiving 2.2
    git-svn-id: http://plugins.svn.wordpress.org/wp-slimstat/tags/2.2@301923 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ options/index.php
@@ -0,0 +1,216 @@
+<?php 
+
+// Avoid direct access to this piece of code
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME'] ) {
+  header('Location: /');
+  exit;
+}
+
+// Load localization files
+load_plugin_textdomain('wp-slimstat-options', WP_PLUGIN_DIR .'/wp-slimstat/lang', '/wp-slimstat/lang');
+
+// Define the panels (true or false if you want the FORM wrapper around your panel)
+$array_panels = array(
+	array(__('General','wp-slimstat-options'), true),
+	array(__('Views','wp-slimstat-options'), true),
+	array(__('Filters','wp-slimstat-options'), true),
+	array(__('Permissions','wp-slimstat-options'), true),
+	array(__('Maintenance','wp-slimstat-options'), false),
+	array(__('Thank you','wp-slimstat-options'), false)
+);
+
+// What panel to display
+$current_panel = empty($_GET['slimpanel'])?1:intval($_GET['slimpanel']);
+
+// Text direction
+if ($wp_locale->text_direction != 'ltr') $array_panels = array_reverse($array_panels, true);
+
+// Update the options
+if (isset($_POST['options'])){
+
+	$faulty_fields = '';
+	if (isset($_POST['options']['is_tracking']) && !slimstat_update_option('slimstat_is_tracking', $_POST['options']['is_tracking'], 'yesno')) $faulty_fields = __('Activate tracking','wp-slimstat-options').', ';
+	if (isset($_POST['options']['enable_javascript']) && !slimstat_update_option('slimstat_enable_javascript', $_POST['options']['enable_javascript'], 'yesno')) $faulty_fields = __('Enable JS Tracking','wp-slimstat-options').', ';
+	if (isset($_POST['options']['custom_js_path']) && !slimstat_update_option('slimstat_custom_js_path', $_POST['options']['custom_js_path'], 'text')) $faulty_fields = __('Custom path','wp-slimstat-options').', ';
+	if (isset($_POST['options']['browscap_autoupdate']) && !slimstat_update_option('slimstat_browscap_autoupdate', $_POST['options']['browscap_autoupdate'], 'yesno')) $faulty_fields = __('Autoupdate Browsers DB','wp-slimstat-options').', ';
+	if (isset($_POST['options']['ignore_interval']) && !slimstat_update_option('slimstat_ignore_interval', $_POST['options']['ignore_interval'], 'integer')) $faulty_fields .= __('Ignore interval','wp-slimstat-options').', ';
+	if (isset($_POST['options']['ignore_bots']) && !slimstat_update_option('slimstat_ignore_bots', $_POST['options']['ignore_bots'], 'yesno')) $faulty_fields .= __('Ignore bots','wp-slimstat-options').', ';
+	if (isset($_POST['options']['track_users']) && !slimstat_update_option('slimstat_track_users', $_POST['options']['track_users'], 'yesno')) $faulty_fields .= __('Track users','wp-slimstat-options').', ';	
+	if (isset($_POST['options']['auto_purge']) && !slimstat_update_option('slimstat_auto_purge', $_POST['options']['auto_purge'], 'integer')) $faulty_fields .= __('Auto purge','wp-slimstat-options').', ';
+	if (isset($_POST['options']['use_separate_menu']) && !slimstat_update_option('slimstat_use_separate_menu', $_POST['options']['use_separate_menu'], 'yesno')) $faulty_fields .= __('Use separate menu','wp-slimstat-options').', ';
+	if (isset($_POST['options']['convert_ip_addresses']) && !slimstat_update_option('slimstat_convert_ip_addresses', $_POST['options']['convert_ip_addresses'], 'yesno')) $faulty_fields .= __('Convert IP addresses','wp-slimstat-options').', ';
+	if (isset($_POST['options']['rows_to_show']) && !slimstat_update_option('slimstat_rows_to_show', $_POST['options']['rows_to_show'], 'integer')) $faulty_fields .= __('Limit results to','wp-slimstat-options').', ';
+	if (isset($_POST['options']['ignore_ip']) && !slimstat_update_option('slimstat_ignore_ip', $_POST['options']['ignore_ip'], 'list')) $faulty_fields .= __('Ignore IPs','wp-slimstat-options').', ';
+	if (isset($_POST['options']['ignore_resources']) && !slimstat_update_option('slimstat_ignore_resources', $_POST['options']['ignore_resources'], 'list')) $faulty_fields .= __('Ignore resources','wp-slimstat-options').', ';
+	if (isset($_POST['options']['ignore_browsers']) && !slimstat_update_option('slimstat_ignore_browsers', $_POST['options']['ignore_browsers'], 'list')) $faulty_fields .= __('Ignore browsers','wp-slimstat-options').', ';
+	if (isset($_POST['options']['ignore_users']) && !slimstat_update_option('slimstat_ignore_users', $_POST['options']['ignore_users'], 'list')) $faulty_fields .= __('Ignore users','wp-slimstat-options').', ';
+	if (isset($_POST['options']['can_view']) && !slimstat_update_option('slimstat_can_view', $_POST['options']['can_view'], 'list')) $faulty_fields .= __('Who can view the reports','wp-slimstat-options').', ';
+	if (isset($_POST['options']['can_admin']) && !slimstat_update_option('slimstat_can_admin', $_POST['options']['can_admin'], 'list')) $faulty_fields .= __('Who can manage these options','wp-slimstat-options').', ';
+	if (isset($_POST['options']['enable_footer_link']) && !slimstat_update_option('slimstat_enable_footer_link', $_POST['options']['enable_footer_link'], 'yesno')) $faulty_fields .= __('Show footer link','wp-slimstat-options').', ';
+	
+	// If the case, delete rows
+	if (isset($_POST['options']['conditional_delete_field']) &&
+		isset($_POST['options']['conditional_delete_operator']) &&
+		isset($_POST['options']['conditional_delete_value']) &&
+		($_POST['options']['conditional_delete_field'] == 'country' ||
+			$_POST['options']['conditional_delete_field'] == 'domain' ||
+			$_POST['options']['conditional_delete_field'] == 'INET_NTOA(ip)' ||
+			$_POST['options']['conditional_delete_field'] == 'language' ||
+			$_POST['options']['conditional_delete_field'] == 'resource' ||
+			$_POST['options']['conditional_delete_field'] == 'searchterms')){
+			$escaped_value = $wpdb->escape($_POST['options']['conditional_delete_value']);
+		switch($_POST['options']['conditional_delete_operator']){
+			case 'equal':
+				$delete_sql = "{$_POST['options']['conditional_delete_field']} = '$escaped_value'";
+				break;
+			case 'like':
+				$delete_sql = "{$_POST['options']['conditional_delete_field']} LIKE '%$escaped_value%'";
+				break;
+			case 'not-like':
+				$delete_sql = "{$_POST['options']['conditional_delete_field']} NOT LIKE '%$escaped_value%'";
+				break;
+			case 'starts-with':
+				$delete_sql = "{$_POST['options']['conditional_delete_field']} LIKE '$escaped_value%'";
+				break;
+			case 'ends-with':
+				$delete_sql = "{$_POST['options']['conditional_delete_field']} LIKE '%$escaped_value'";
+				break;
+			case 'does-not-start-with':
+				$delete_sql = "{$_POST['options']['conditional_delete_field']} NOT LIKE '$escaped_value%'";
+				break;
+			case 'does-not-end-with':
+				$delete_sql = "{$_POST['options']['conditional_delete_field']} NOT LIKE '%$escaped_value'";
+				break;
+			
+		}
+		$rows_affected =  $wpdb->query("DELETE FROM `{$table_prefix}slim_stats` WHERE $delete_sql");
+		$message_to_show = __('Your WP SlimStat table has been successfully cleaned. Rows affected:','wp-slimstat-options').' '.intval($rows_affected);
+	}
+	
+	// If autopurge = 0, we can unschedule our cron job. If autopurge > 0 and the hook was not scheduled, we schedule it
+	if (isset($_POST['options']['auto_purge'])){
+		if ($_POST['options']['auto_purge'] == 0){
+			wp_clear_scheduled_hook('wp_slimstat_purge');
+		}
+		else if (wp_next_scheduled( 'my_schedule_hook' ) == 0){
+			wp_schedule_event(time(), 'daily', 'wp_slimstat_purge');
+		}
+	}
+	// Display an alert in the admin interface if something went wrong
+	echo '<div id="wp-slimstat-message" class="updated fade"><p>';
+	if (empty($faulty_fields)){
+		if (empty($message_to_show)){
+			_e('Your settings have been successfully updated.','wp-slimstat-options');
+		}
+		else{
+			echo $message_to_show;
+		}
+	}
+	else{
+		_e('There was an error updating the following fields:','wp-slimstat-options');
+		echo ' <strong>'.substr($faulty_fields,0,-2).'</strong>';
+	}
+	echo "</p></div>\n";
+}
+
+function slimstat_update_option( $_option, $_value, $_type ){
+	if (!isset($_value)) return true;
+
+	switch($_type){
+		case 'list':
+			// Avoid XSS attacks
+			$clean_value = preg_replace('/[^a-zA-Z0-9\,\.\/]/', '', $_value);
+			if (strlen($_value)==0){
+				update_option($_option, array());
+			}
+			else {
+				$array_values = explode(',',$clean_value);
+				update_option($_option, $array_values);
+			}
+			
+			return true;
+			break;
+		case 'yesno':
+			if ($_value=='yes' || $_value=='no'){
+				update_option($_option, $_value);
+				return true;
+			}
+			
+			break;
+		case 'integer':
+			update_option($_option, abs(intval($_value)));
+			
+			return true;
+			break;
+			
+		default:
+			update_option($_option, strip_tags($_value));
+			return true;
+			break;
+	}
+	
+	return false;
+}
+
+?>
+
+<div class="wrap">
+	<div id="analytics-icon" class="<?php echo $wp_locale->text_direction ?>"></div>
+	<h2 class="medium">
+		<?php
+		foreach($array_panels as $a_panel_id => $a_panel_details){
+			echo '<a class="menu-tabs';
+			if ($current_panel != $a_panel_id+1) echo ' menu-tab-inactive';
+			echo '" href="admin.php?page=wp-slimstat/options/index.php&slimpanel='.($a_panel_id+1).'">'.$a_panel_details[0].'</a>';
+		}
+		?>
+	</h2>
+
+	<?php
+		if (isset($_GET['ds']) || isset($_GET['di2c'])){
+			echo '<div id="wp-slimstat-message" class="updated fade"><p>';
+			if (isset($_GET['ds']) && $_GET['ds']=='yes'){
+				_e('Are you sure you want to remove all the information about your hits and visits?','wp-slimstat-options');
+				echo ' <a class="button-secondary" href="?page=wp-slimstat/options/index.php&ds=confirm">'.__('Yes','wp-slimstat-options').'</a>';
+				echo ' <a class="button-secondary" href="?page=wp-slimstat/options/index.php">'.__('No','wp-slimstat-options').'</a>';
+			}
+			if (isset($_GET['ds']) && $_GET['ds']=='confirm'){
+				$wpdb->query("TRUNCATE TABLE `{$table_prefix}slim_stats`");
+				$wpdb->query("TRUNCATE TABLE `{$table_prefix}slim_visits`");
+				_e('Your WP SlimStat table has been successfully emptied.','wp-slimstat-options');
+			}
+			if (isset($_GET['di2c']) && $_GET['di2c']=='yes'){
+				_e('Are you sure you want to drop the ip-to-countries table?','wp-slimstat-options');
+				echo ' <a class="button-secondary" href="?page=wp-slimstat/options/index.php&di2c=confirm">'.__('Yes','wp-slimstat-options').'</a>';
+				echo ' <a class="button-secondary" href="?page=wp-slimstat/options/index.php">'.__('No','wp-slimstat-options').'</a>';
+			}
+			if (isset($_GET['di2c']) && $_GET['di2c']=='confirm'){
+				$wpdb->query("TRUNCATE TABLE `{$table_prefix}slim_countries`");
+				_e('Your IP-to-countries table has been successfully emptied. Now go to your Plugins panel and deactivate/reactivate WP SlimStat to load the new data.','wp-slimstat-options');
+			}
+			echo '</p></div>';
+		}
+		if (isset($_GET['rs']) && $_GET['rs']=='yes'){
+			if (!isset($wp_slimstat_object)) $wp_slimstat_object = new wp_slimstat();
+			$wpdb->query("DROP TABLE IF EXISTS `$wp_slimstat_object->table_stats`");
+			echo '<div id="wp-slimstat-message" class="updated fade"><p>';
+			_e('Your WP SlimStat table has been successfully reset. Now go to your Plugins panel and deactivate/reactivate WP SlimStat.','wp-slimstat-options');		
+			echo '</p></div>';
+		}
+		if (isset($_GET['ot']) && $_GET['ot']=='yes'){
+			if (!isset($wp_slimstat_object)) $wp_slimstat_object = new wp_slimstat();
+			$wpdb->query("OPTIMIZE TABLE `$wp_slimstat_object->table_stats`");
+			echo '<div id="wp-slimstat-message" class="updated fade"><p>';
+			_e('Your WP SlimStat table has been successfully optimized.','wp-slimstat-options');		
+			echo '</p></div>';
+		}
+	?>
+	
+	<?php if ($array_panels[$current_panel-1][1]) { ?><form action="admin.php?page=wp-slimstat/options/index.php<?php if(!empty($_GET['slimpanel'])) echo '&slimpanel='.$_GET['slimpanel']; ?>" method="post"><?php } ?>
+	
+	<?php if (is_readable(WP_PLUGIN_DIR."/wp-slimstat/options/panel$current_panel.php")) require_once(WP_PLUGIN_DIR."/wp-slimstat/options/panel$current_panel.php"); ?>
+	
+	<?php if ($array_panels[$current_panel-1][1]) { ?><p class="submit"><input type="submit" value="<?php _e('Save Changes') ?>" class="button-primary" name="Submit"></p><?php } ?>
+
+<?php if ($array_panels[$current_panel-1][1]) { ?></form><?php } ?>
+</div>
---
+++ readme.txt
@@ -0,0 +1,110 @@
+=== WP SlimStat ===
+
+Donate link: http://www.duechiacchiere.it/wp-slimstat
+Tags: analytics, statistics, slimstat, shortstat, tracking, pathstat, reports, referers, hits, pageviews, world map, stats, maxmind, fusion charts
+Requires at least: 2.9.2
+Tested up to: 3.1
+Stable tag: 2.2
+
+== Description ==
+A simple but powerful real-time web analytics plugin for Wordpress. It doesn't require any subscription
+to external statistic services: all metrics are kept on your local server, private and accessible
+to your eyes only. Features the famous one-click install-and-go.
+
+## Requirements
+* Wordpress 2.9 or higher
+* PHP 5.1 or higher
+* MySQL 5.x or higher
+* At least 5 MB of free web space
+* At least 5 MB of free DB space
+
+## Main Features
+* It tracks visits (user sessions up to 30 minutes) as defined by Google Analytics
+* It tracks outbound links and downloads
+* It tracks internal searches
+* It tracks screen resolutions and other browser-related parameters
+* It works with [W3 Total Cache](http://lab.duechiacchiere.it/index.php?topic=45.0)!
+* It exports a simple API to let you develop your own reports
+* Fully localizable in your language (please contribute!)
+* The best country, browser and platform detection ever seen (thanks to [Browscap](http://code.google.com/p/phpbrowscap/) and [MaxMind](http://www.maxmind.com/) )
+* Real-time charts to help you visualize your visitors' trends (thanks to [FusionCharts](http://www.fusioncharts.com/free/) free edition)
+* The browser recognition database is automatically updated every 2 weeks
+* It shows detailed information about each visitor: Country, city, area, organization, etc (thanks to [IP2Location](http://www.ip2location.com) )
+* 10,000 pageviews use just 1.4 megabytes of DB space
+* It ignores pageviews based on IP addresses, browsers, referrers, users and permalinks
+* You can restrict view/admin access to specific users
+* It uses WP timezone settings and date formatting
+* It has a lot of filters for content drill-down
+* It includes a detailed World Map
+
+== Installation ==
+
+1. Upload the entire folder and all the subfolders to your Wordpress plugins' folder
+2. Activate it
+3. Make sure your template calls `wp_footer()` or the equivalent hook somewhere (possibly just before the `</body>` tag)
+4. You'll see a new entry under the Dashboard menu
+5. To customize all its options, go to Settings > Slimstat
+6. Show your appreciation! Tell your friends you are using WP SlimStat, write an article about it!
+
+== Screenshots ==
+
+1. Reports
+2. Options
+
+== Changelog ==
+
+= 2.2 =
+* WP SlimStat is now faster than ever! Most SQL queries have been optimized and are now able to better leverage indexes
+* Multisite aware - Perform network installations in a few clicks (highly experimental, I need volunteer to test it!). The [underlying functions](http://core.trac.wordpress.org/ticket/14170#comment:22) are being discussed by WP Core developers, so this may change in the foreseeable future. Please don't test this feature on a production environment!
+* Javascript code is now minified (thank you Google Page Speed)
+* Added a new option to track legged in WP users
+* Added a new option to filter metrics by post category ID
+* Visitor Tracking (via cookie) is now PHP-based, not Javascript-based anymore, making it even more reliable
+* Improved the way visitors are counted in some metrics
+* Fixed some more warning errors that came out with PHP in 'strict' mode (thank you Julien Schmidt)
+* Fixed a bug that prevented the correct display of long times frames, spanning through multiple months
+* Fixed a bug that prevented filtering by search terms containing single and double quotes
+* Geolocalization: updated the information in the CSV file included (October 2010). Go to Options > Maintenance > Reset Ip-to-Countries. Then deactivate/reactivate WP SlimStat to import the new file.
+
+= 2.1 =
+* Fixed a bug that prevented Visitors to be tracked properly. If you downloaded/installed WP SlimStat 2.0.9, go to Settings > WP SlimStat > General tab and (unless you have  explicitly changed this option) set the value for `Custom Path` to the default one shown in the description underneath. 
+* Fixed a bug that led to inaccurate operating systems detection (thank you Herman)
+* WP SlimStat speaks French! Thank you Tuxicoman!
+* Updated browscap.ini and cache.php to the latest version available
+
+= 2.0.9 =
+* A new member has joined the WP SlimStat Family: [WP SlimStat ShortCodes](http://wordpress.org/extend/plugins/wp-slimstat-shortcodes/)
+* Improved the Options > Maintenance panel: now you can delete rows based on the IP address, and use some new filters (thank you Herman)
+* Now you can filter metrics [based on authors](http://lab.duechiacchiere.it/index.php?topic=33.0) (thank you Matt)
+* A new option under Options > General allows you to move `wp-slimstat-js.php` out of its original folder, for security reasons (thank you Steven). You will have to edit this file accordingly, though, before moving it
+* Added a new option to filter hits based on users being logged in (Options > Filters)
+* Added a new option to optimize your database, if needed (Options > Maintenance)
+* Fixed a bug that prevented links with target=_blank to open in a new window (thank you Julius and [Wordpress Deutschland](http://forum.wordpress-deutschland.org/konfiguration/71929-ext-links-oeffnen-sich-im-gleichen-fenster-trotz-_blank-2.html))
+* Fixed a typo that prevented one of the icons to show up correctly
+* Fixed a problem with BrowsCap that occurred when the cache folder was not writeable (thank you Julius)
+* Fixed some warning errors that came out with PHP in 'strict' mode (error reporting = E_ALL and WP_DEBUG = true, thank you Olivier Sermann)
+* Bounce rate percentages are now even more accurate
+* Geolocalization: updated the information in the CSV file included (August 2010). Go to Options > Maintenance > Reset Ip-to-Countries. Then deactivate/reactivate WP SlimStat to import the new file.
+
+== Localization ==
+
+Wp-SlimStat is fully localizable. Everything can be translate in your language
+and character encoding. As for every other Wordpress plugin, I used the Portable
+Object (.po) standard to do this. If you want to provide a localized file in your
+language, use the template files (.pot) you'll find inside the `lang` folder.
+
+== List of donors in alphabetical order ==
+* [Andrea Pinti](http://andreapinti.com/)
+* [Bluewave Blog](http://blog.bluewaveweb.co.uk/)
+* [Herman Peet](http://www.hermanpeet.nl/)
+* [La casetta delle pesche](http://www.lacasettadellepesche.it/)
+* Mora Systems
+
+== Contributors in alphabetical order ==
+* [Digo](http://www.showhypnose.org/blog/) - German localization
+* [Tuxicoman](http://tuxicoman.jesuislibre.net/) - French localization
+
+= Dashboard Widgets = 
+After you download and install WP SlimStat, you'll see not one, but two new plugins in your administration panel.
+Don't worry, you just need to activate the first one in order to track your visitors. WP SlimStat Dashboard Widgets
+adds some reports directly to your dashboard. Useful for those who want the most relevant metrics handy.
