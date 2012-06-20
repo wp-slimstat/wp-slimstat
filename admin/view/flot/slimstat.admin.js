becab835479ad7086fc30552084e393bf47a2fde
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-20 18:53:07 +0000

    Version 2.8.1
    git-svn-id: http://plugins.svn.wordpress.org/wp-slimstat/tags/2.8.1@561276 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/view/flot/slimstat.admin.js
@@ -0,0 +1,127 @@
+﻿function showTooltip(x, y, content, class_label, minus){
+	var class_attribute = class_label ? ' class="'+class_label+'"':'';
+	jQuery('<div id="jquery-tooltip"' + class_attribute + '>' + content + '</div>').appendTo("body");
+	jQuery("#jquery-tooltip").css({
+		top:y+10,
+		left:minus?x-jQuery("#jquery-tooltip").width()-10:x+10,
+	}).fadeIn(200);
+}
+function tickFormatter(n){
+	n += '';
+	x = n.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? decimalPoint + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + thousandsSeparator + '$2');
+	}
+	return x1 + x2;
+}
+function color_weekends(){
+	if (!slimstat_day_filter_active) jQuery(".xAxis .tickLabel").each(function(i){
+		myDate = new Date(slimstat_current_year, slimstat_current_month-1, parseInt(jQuery(this).html()), 3, 30, 0);
+		if(myDate.getDay()%6 == 0) jQuery(this).css('color','#ccc');
+	});
+}
+var previousPoint = null;
+
+jQuery(document).ready(function(){
+	jQuery('.box-help').hover(
+			function(event){
+				element_class = (this.id.length > 0)?'tooltip-'+this.id:'tooltip-fixed-width';
+				this.savetitle = this.title;
+				showTooltip(event.pageX, event.pageY, this.title, element_class, 'yes');
+				this.title = '';
+			},
+			function(){
+				this.title = this.savetitle;
+				jQuery('#jquery-tooltip').remove();
+			}
+	);
+
+	jQuery('.inline-help').hover(
+			function(event){
+				this.savetitle = this.title;
+				showTooltip(event.pageX, event.pageY, this.title, 'tooltip-fixed-width');
+				this.title = '';
+			},
+			function(){
+				this.title = this.savetitle;
+				jQuery('#jquery-tooltip').remove();
+			}
+	);
+
+	// Slimstat Dashboard CSS Tweaks
+	jQuery('[id^=slim_]').addClass('slimstat');
+	jQuery('#slim_p1_01').removeClass('slimstat');
+
+	jQuery('.slimstat .inside p:not(.header)').hover(
+		function(){
+			if (this.title.length > 0){
+				this.savetitle = this.title;
+				jQuery(this).append('<b id="wp-element-details">'+this.title+'</b>');
+				this.title = '';
+			}
+		},
+		function(){
+			if (jQuery('#wp-element-details').is('*')){
+				this.title = this.savetitle;
+				jQuery('#wp-element-details').remove();
+			}
+		}
+	);
+
+	jQuery('input.hide-postbox-tog[id^=slim_]').bind('click.postboxes', function (){
+		var box_id = '#'+jQuery(this).val();
+		if (typeof slimstat_filters_string == 'undefined') slimstat_filters_string = '';
+		jQuery(box_id+' .inside').html('<p class="nodata loading"></p>');
+		var data = {action: 'slimstat_load_report', fs: slimstat_filters_string, box_id: box_id, security: jQuery('#meta-box-order-nonce').val()}
+		if (jQuery(this).prop("checked") && jQuery('#'+jQuery(this).val()).length !== 0){
+			jQuery.post(ajaxurl, data, function(response){
+				jQuery(box_id+' .inside').html(response);
+				jQuery(box_id+' .inside p:not(.header)').hover(
+					function(){
+						if (this.title.length > 0){
+							this.savetitle = this.title;
+							jQuery(this).append('<b id="wp-element-details">'+this.title+'</b>');
+							this.title = '';
+						}
+					},
+					function(){
+						if (jQuery('#wp-element-details').is('*')){
+							this.title = this.savetitle;
+							jQuery('#wp-element-details').remove();
+						}
+					}
+				);
+			});
+		}
+	});
+	
+
+	
+	jQuery('#more-dialog').dialog({
+		modal : true,
+		autoOpen : false,
+		closeOnEscape : true,
+		draggable : false,
+		open: function(){
+            jQuery('.ui-widget-overlay').bind('click',function(){
+                jQuery('#more-dialog').dialog('close');
+            })
+        },
+		resizable : false,
+		width : '760'
+//		'buttons'       : {
+//			"Close": function() {
+//				jQuery(this).dialog('close');
+//			}
+//		}
+	});
+	jQuery('.more').click(function(event) {
+		event.preventDefault();
+		jQuery('#more-dialog').dialog('open');
+	});
+	
+	
+});
---
+++ admin/view/flot/slimstat.chart.js
@@ -0,0 +1,70 @@
+﻿var placeholder = jQuery("#chart-placeholder");
+var options = {
+	zoom:{interactive:true},
+	pan:{interactive:true},
+	series:{
+		lines:{show:true},
+		points:{
+			show:true,
+			symbol:function(ctx, x, y, radius, shadow){ctx.arc(x, y, 2, 0, Math.PI * 2, false)} 
+		},
+		colors:[{opacity:0.85}],
+		shadowSize:5
+	},
+	colors:['#ccc','#21759B','#02c907'],
+	shadowSize:0,
+	xaxis:{
+		tickSize:1,
+		tickDecimals:0,
+		ticks:(slimstat_interval>0 && window.ticks.length>20)?[]:window.ticks,
+		zoomRange:[5,window.ticks.length],
+		panRange:[0,window.ticks.length]
+	},
+	yaxis:{
+		tickDecimals:0,
+		zoomRange:[5, slimstat_max_yaxis+parseInt(slimstat_max_yaxis/5)],
+		panRange:[0, slimstat_max_yaxis+parseInt(slimstat_max_yaxis/5)]
+	},
+	grid:{
+		backgroundColor:"#ffffff",
+		borderWidth:0,
+		hoverable:true,
+		clickable:true
+	},
+	legend:{
+		container:"#chart-legend",
+		noColumns:3
+	}
+};
+jQuery.plot(placeholder, [slimstat_a, slimstat_b, slimstat_c], options);
+color_weekends();
+placeholder.bind('plothover', function(event, pos, item){
+	if (item){
+		if (previousPoint != item.dataIndex){
+			previousPoint = item.dataIndex;
+			jQuery("#jquery-tooltip").remove();
+			showTooltip(item.pageX, item.pageY, item.series.label+': <b>'+window.ticks[item.datapoint[0]][1]+'</b> = '+tickFormatter(item.datapoint[1]));
+		}
+	}
+	else{
+		jQuery('#jquery-tooltip').remove();
+		previousPoint = null;            
+	}
+});
+placeholder.bind('plotclick', function(event, pos, item){
+	if (item){
+		if (item.seriesIndex == 1 && typeof(window.chart_data[item.seriesIndex][item.datapoint[0]-slimstat_rtl_filler_previous][2]) != 'undefined'){
+			document.location.href = window.location.pathname+'?page=wp-slimstat&fs='+window.chart_data[item.seriesIndex][item.datapoint[0]-slimstat_rtl_filler_previous][2];
+		}
+		if (item.seriesIndex != 1 && typeof(window.chart_data[item.seriesIndex][item.datapoint[0]-slimstat_rtl_filler_current][2]) != 'undefined'){
+			document.location.href = window.location.pathname+'?page=wp-slimstat&fs='+window.chart_data[item.seriesIndex][item.datapoint[0]-slimstat_rtl_filler_current][2];
+			
+		}
+	}
+});
+placeholder.bind('dblclick', function(event){
+	jQuery.plot(placeholder, [slimstat_a, slimstat_b, slimstat_c], options);
+	color_weekends();
+});
+placeholder.bind('plotzoom', color_weekends);
+placeholder.bind('plotpan', color_weekends);
