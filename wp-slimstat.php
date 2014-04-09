2b0225200073fd1cf5ec03ad4155c0631ad7e7db
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-09 13:52:20 +0000

    Version 3.5.9 - Fixed bug that was causing loss of data in some specific cases
    git-svn-id: http://plugins.svn.wordpress.org/wp-slimstat/trunk@890094 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-slimstat.php
+++ wp-slimstat.php
@@ -3,7 +3,7 @@
 Plugin Name: WP SlimStat
 Plugin URI: http://wordpress.org/plugins/wp-slimstat/
 Description: The most accurate real-time statistics plugin for WordPress
-Version: 3.5.8
+Version: 3.5.9
 Author: Camu
 Author URI: http://slimstat.getused.to.it/
 */
@@ -11,7 +11,7 @@ Author URI: http://slimstat.getused.to.it/
 if (!empty(wp_slimstat::$options)) return true;
 
 class wp_slimstat{
-	public static $version = '3.5.8';
+	public static $version = '3.5.9';
 	public static $options = array();
 	
 	public static $wpdb = '';
