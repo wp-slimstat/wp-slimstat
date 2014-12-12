e226ce52f04761a5392f8a253d674bdfb9d9776c
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 13:43:44 +0000

    Version 3.8.6 - Getting ready for new release
    git-svn-id: http://plugins.svn.wordpress.org/wp-slimstat/tags/3.8.6@1043459 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/js/slimstat.config.admin.js
@@ -0,0 +1,8 @@
+﻿jQuery(function(){
+	jQuery('#use_separate_menu_yes').click(function(){
+		jQuery('#form-slimstat-options-tab-1').attr('action', 'admin.php?page=wp-slim-config&tab=1');
+	});
+	jQuery('#use_separate_menu_no').click(function(){
+		jQuery('#form-slimstat-options-tab-1').attr('action', 'admin.php?page=wp-slim-config&tab=1');
+	});
+});
