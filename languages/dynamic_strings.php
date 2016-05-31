7535e4d2ba39020fcfd04b4cb55626cee8998f67
coolmann <coolmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2016-05-31 00:41:52 +0000

    Version 4.3.2.3
    git-svn-id: http://plugins.svn.wordpress.org/wp-slimstat/tags/4.3.2.3@1427199 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ languages/dynamic_strings.php
@@ -0,0 +1,344 @@
+<?php 
+$dynamic_strings = array(
+__('xx','wp-slimstat'),
+
+// Operating Systems
+__('aix','wp-slimstat'), // IBM AIX
+__('amiga','wp-slimstat'), // Amiga
+__('android','wp-slimstat'), // Android
+__('beos','wp-slimstat'), // BeOS
+__('blackberry os','wp-slimstat'), // BlackBerry OS
+__('centos','wp-slimstat'), // CentOS
+__('chromeos','wp-slimstat'), // ChromeOS
+__('commodore64','wp-slimstat'), // Commodore 64
+__('cygwin','wp-slimstat'), // Cygwin
+__('debian','wp-slimstat'), // Debian
+__('digital unix','wp-slimstat'), // Digital Unix
+__('fedora','wp-slimstat'), // Fedora
+__('firefoxos','wp-slimstat'), // Firefox OS
+__('freebsd','wp-slimstat'), // FreeBSD
+__('gentoo','wp-slimstat'), // Gentoo
+__('hp-ux','wp-slimstat'), // HP-UX
+__('ios','wp-slimstat'), // iPhone OS
+__('iphone os','wp-slimstat'), // iPhone OS
+__('iphone osx','wp-slimstat'), // iPhone OS X
+__('irix','wp-slimstat'), // SGI / IRIX
+__('java','wp-slimstat'), // Java
+__('kanotix','wp-slimstat'), // Kanotix Linux
+__('knoppix','wp-slimstat'), // Knoppix Linux
+__('linux','wp-slimstat'), // Linux Generic
+__('mac','wp-slimstat'), // Mac
+__('mac68k','wp-slimstat'), // Mac 68k
+__('macosx','wp-slimstat'), // Mac OS X
+__('macppc','wp-slimstat'), // Mac PowerPC
+__('mandrake','wp-slimstat'), // Mandrake Linux
+__('mandriva','wp-slimstat'), // MS-DOS
+__('mepis','wp-slimstat'), // MEPIS Linux
+__('ms-dos','wp-slimstat'), // MS-DOS
+__('netbsd','wp-slimstat'), // NetBSD
+__('nintendo','wp-slimstat'), // Nintendo
+__('openbsd','wp-slimstat'), // OpenBSD
+__('openvms','wp-slimstat'), // OpenVMS
+__('os/2','wp-slimstat'), // IBM OS/2
+__('palm','wp-slimstat'), // Palm OS
+__('palmos','wp-slimstat'), // Palm OS
+__('pclinuxos','wp-slimstat'), // PCLinux OS
+__('playstation','wp-slimstat'), // Playstation
+__('powertv','wp-slimstat'), // PowerTV
+__('redhat','wp-slimstat'), // RedHat Linux
+__('rim os','wp-slimstat'), // Blackberry
+__('risc os','wp-slimstat'), // Risc OS
+__('slackware','wp-slimstat'), // Slackware Linux
+__('solaris','wp-slimstat'), // Solaris
+__('sunos','wp-slimstat'), // Sun OS
+__('suse','wp-slimstat'), // SuSE Linux
+__('symbianos','wp-slimstat'), // Symbian OS
+__('ubuntu','wp-slimstat'), // Java
+__('unix','wp-slimstat'), // Unix
+__('unknown','wp-slimstat'), // Unknown
+__('xandros','wp-slimstat'), // Xandros Linux
+__('wap','wp-slimstat'), // WAP
+__('webos','wp-slimstat'), // WebOS
+__('win10','wp-slimstat'), // Windows 10
+__('win16','wp-slimstat'), // Windows 16-bit
+__('win2000','wp-slimstat'), // Windows 2000
+__('win2003','wp-slimstat'), // Windows 2003
+__('win31','wp-slimstat'), // Windows 3.1
+__('win32','wp-slimstat'), // Windows 32-bit
+__('win7','wp-slimstat'), // Windows 7
+__('win7','wp-slimstat'), // Windows 7
+__('win8','wp-slimstat'), // Windows 8
+__('win8.1','wp-slimstat'), // Windows 8.1
+__('win95','wp-slimstat'), // Windows 95
+__('win98','wp-slimstat'), // Windows 98
+__('wince','wp-slimstat'), // Windows CE
+__('winme','wp-slimstat'), // Windows ME
+__('winnt','wp-slimstat'), // Windows NT
+__('winphone7','wp-slimstat'), // Windows Phone
+__('winphone7.5','wp-slimstat'), // Windows Phone
+__('winphone8','wp-slimstat'), // Windows Phone
+__('winphone8.1','wp-slimstat'), // Windows RT / Runtime
+__('winrt','wp-slimstat'), // Windows Phone
+__('winvista','wp-slimstat'), // Windows Vista
+__('winxp','wp-slimstat'), // Windows XP
+__('wyderos','wp-slimstat'), // WyderOS
+__('zaurus','wp-slimstat'), // Zaurus WAP
+
+// Browser Extensions
+__('acrobat','wp-slimstat'), // Acrobat Reader
+__('director','wp-slimstat'), // Macromedia Director
+__('flash','wp-slimstat'), // Adobe Flash Player
+__('mediaplayer','wp-slimstat'), // Microsoft Media Player
+__('quicktime','wp-slimstat'), // QuickTime
+__('real','wp-slimstat'), // Real Player
+__('silverlight','wp-slimstat'), // Microsoft Silverlight
+
+// Operating System Families
+__('p-and','wp-slimstat'), // Android
+__('p-bla','wp-slimstat'), // BlackBerry
+__('p-chr','wp-slimstat'), // Chrome OS
+__('p-fir','wp-slimstat'), // Fire OS
+__('p-fre','wp-slimstat'), // Linux FreeBSD
+__('p-ios','wp-slimstat'), // Apple iOS
+__('p-jav','wp-slimstat'), // Java-based OS
+__('p-lin','wp-slimstat'), // Linux
+__('p-mac','wp-slimstat'), // Apple
+__('p-rim','wp-slimstat'), // Blackberry
+__('p-sym','wp-slimstat'), // Symbian OS
+__('p-ubu','wp-slimstat'), // Linux
+__('p-unk','wp-slimstat'), // Unknown
+__('p-win','wp-slimstat'), // Microsoft
+
+// Languages
+__('l-af','wp-slimstat'), // Afrikaans
+__('l-af-za','wp-slimstat'), // Afrikaans (South Africa)
+__('l-ar','wp-slimstat'), // Arabic
+__('l-ar-ae','wp-slimstat'), // Arabic (U.A.E.)
+__('l-ar-bh','wp-slimstat'), // Arabic (Bahrain)
+__('l-ar-dz','wp-slimstat'), // Arabic (Algeria)
+__('l-ar-eg','wp-slimstat'), // Arabic (Egypt)
+__('l-ar-iq','wp-slimstat'), // Arabic (Iraq)
+__('l-ar-jo','wp-slimstat'), // Arabic (Jordan)
+__('l-ar-kw','wp-slimstat'), // Arabic (Kuwait)
+__('l-ar-lb','wp-slimstat'), // Arabic (Lebanon)
+__('l-ar-ly','wp-slimstat'), // Arabic (Libya)
+__('l-ar-ma','wp-slimstat'), // Arabic (Morocco)
+__('l-ar-om','wp-slimstat'), // Arabic (Oman)
+__('l-ar-qa','wp-slimstat'), // Arabic (Qatar)
+__('l-ar-sa','wp-slimstat'), // Arabic (Saudi Arabia)
+__('l-ar-sy','wp-slimstat'), // Arabic (Syria)
+__('l-ar-tn','wp-slimstat'), // Arabic (Tunisia)
+__('l-ar-ye','wp-slimstat'), // Arabic (Yemen)
+__('l-az','wp-slimstat'), // Azeri (Latin)
+__('l-az-az','wp-slimstat'), // Azeri (Latin) (Azerbaijan)
+__('l-be','wp-slimstat'), // Belarusian
+__('l-be-by','wp-slimstat'), // Belarusian (Belarus)
+__('l-bg','wp-slimstat'), // Bulgarian
+__('l-bg-bg','wp-slimstat'), // Bulgarian (Bulgaria)
+__('l-bs-ba','wp-slimstat'), // Bosnian (Bosnia and Herzegovina)
+__('l-ca','wp-slimstat'), // Catalan
+__('l-ca-es','wp-slimstat'), // Catalan (Spain)
+__('l-cs','wp-slimstat'), // Czech
+__('l-cs-cz','wp-slimstat'), // Czech (Czech Republic)
+__('l-cy','wp-slimstat'), // Welsh
+__('l-cy-gb','wp-slimstat'), // Welsh (United Kingdom)
+__('l-da','wp-slimstat'), // Danish
+__('l-da-dk','wp-slimstat'), // Danish (Denmark)
+__('l-de','wp-slimstat'), // German
+__('l-de-at','wp-slimstat'), // German (Austria)
+__('l-de-ch','wp-slimstat'), // German (Switzerland)
+__('l-de-de','wp-slimstat'), // German (Germany)
+__('l-de-li','wp-slimstat'), // German (Liechtenstein)
+__('l-de-lu','wp-slimstat'), // German (Luxembourg)
+__('l-dv','wp-slimstat'), // Divehi
+__('l-dv-mv','wp-slimstat'), // Divehi (Maldives)
+__('l-el','wp-slimstat'), // Greek
+__('l-el-gr','wp-slimstat'), // Greek (Greece)
+__('l-en','wp-slimstat'), // English
+__('l-en-au','wp-slimstat'), // English (Australia)
+__('l-en-bz','wp-slimstat'), // English (Belize)
+__('l-en-ca','wp-slimstat'), // English (Canada)
+__('l-en-cb','wp-slimstat'), // English (Caribbean)
+__('l-en-gb','wp-slimstat'), // English (United Kingdom)
+__('l-en-ie','wp-slimstat'), // English (Ireland)
+__('l-en-jm','wp-slimstat'), // English (Jamaica)
+__('l-en-nz','wp-slimstat'), // English (New Zealand)
+__('l-en-ph','wp-slimstat'), // English (Republic of the Philippines)
+__('l-en-tt','wp-slimstat'), // English (Trinidad and Tobago)
+__('l-en-us','wp-slimstat'), // English (United States)
+__('l-en-za','wp-slimstat'), // English (South Africa)
+__('l-en-zw','wp-slimstat'), // English (Zimbabwe)
+__('l-eo','wp-slimstat'), // Esperanto
+__('l-es','wp-slimstat'), // Spanish
+__('l-es-ar','wp-slimstat'), // Spanish (Argentina)
+__('l-es-bo','wp-slimstat'), // Spanish (Bolivia)
+__('l-es-cl','wp-slimstat'), // Spanish (Chile)
+__('l-es-co','wp-slimstat'), // Spanish (Colombia)
+__('l-es-cr','wp-slimstat'), // Spanish (Costa Rica)
+__('l-es-do','wp-slimstat'), // Spanish (Dominican Republic)
+__('l-es-ec','wp-slimstat'), // Spanish (Ecuador)
+__('l-es-es','wp-slimstat'), // Spanish (Spain)
+__('l-es-gt','wp-slimstat'), // Spanish (Guatemala)
+__('l-es-hn','wp-slimstat'), // Spanish (Honduras)
+__('l-es-mx','wp-slimstat'), // Spanish (Mexico)
+__('l-es-ni','wp-slimstat'), // Spanish (Nicaragua)
+__('l-es-pa','wp-slimstat'), // Spanish (Panama)
+__('l-es-pe','wp-slimstat'), // Spanish (Peru)
+__('l-es-pr','wp-slimstat'), // Spanish (Puerto Rico)
+__('l-es-py','wp-slimstat'), // Spanish (Paraguay)
+__('l-es-sv','wp-slimstat'), // Spanish (El Salvador)
+__('l-es-uy','wp-slimstat'), // Spanish (Uruguay)
+__('l-es-ve','wp-slimstat'), // Spanish (Venezuela)
+__('l-et','wp-slimstat'), // Estonian
+__('l-et-ee','wp-slimstat'), // Estonian (Estonia)
+__('l-eu','wp-slimstat'), // Basque
+__('l-eu-es','wp-slimstat'), // Basque (Spain)
+__('l-fa','wp-slimstat'), // Farsi
+__('l-fa-ir','wp-slimstat'), // Farsi (Iran)
+__('l-fi','wp-slimstat'), // Finnish
+__('l-fi-fi','wp-slimstat'), // Finnish (Finland)
+__('l-fo','wp-slimstat'), // Faroese
+__('l-fo-fo','wp-slimstat'), // Faroese (Faroe Islands)
+__('l-fr','wp-slimstat'), // French
+__('l-fr-be','wp-slimstat'), // French (Belgium)
+__('l-fr-ca','wp-slimstat'), // French (Canada)
+__('l-fr-ch','wp-slimstat'), // French (Switzerland)
+__('l-fr-fr','wp-slimstat'), // French (France)
+__('l-fr-lu','wp-slimstat'), // French (Luxembourg)
+__('l-fr-mc','wp-slimstat'), // French (Principality of Monaco)
+__('l-gl','wp-slimstat'), // Galician
+__('l-gl-es','wp-slimstat'), // Galician (Spain)
+__('l-gu','wp-slimstat'), // Gujarati
+__('l-gu-in','wp-slimstat'), // Gujarati (India)
+__('l-he','wp-slimstat'), // Hebrew
+__('l-he-il','wp-slimstat'), // Hebrew (Israel)
+__('l-hi','wp-slimstat'), // Hindi
+__('l-hi-in','wp-slimstat'), // Hindi (India)
+__('l-hr','wp-slimstat'), // Croatian
+__('l-hr-ba','wp-slimstat'), // Croatian (Bosnia and Herzegovina)
+__('l-hr-hr','wp-slimstat'), // Croatian (Croatia)
+__('l-hu','wp-slimstat'), // Hungarian
+__('l-hu-hu','wp-slimstat'), // Hungarian (Hungary)
+__('l-hy','wp-slimstat'), // Armenian
+__('l-hy-am','wp-slimstat'), // Armenian (Armenia)
+__('l-id','wp-slimstat'), // Indonesian
+__('l-id-id','wp-slimstat'), // Indonesian (Indonesia)
+__('l-is','wp-slimstat'), // Icelandic
+__('l-is-is','wp-slimstat'), // Icelandic (Iceland)
+__('l-it','wp-slimstat'), // Italian
+__('l-it-ch','wp-slimstat'), // Italian (Switzerland)
+__('l-it-it','wp-slimstat'), // Italian (Italy)
+__('l-ja','wp-slimstat'), // Japanese
+__('l-ja-jp','wp-slimstat'), // Japanese (Japan)
+__('l-ka','wp-slimstat'), // Georgian
+__('l-ka-ge','wp-slimstat'), // Georgian (Georgia)
+__('l-kk','wp-slimstat'), // Kazakh
+__('l-kk-kz','wp-slimstat'), // Kazakh (Kazakhstan)
+__('l-kn','wp-slimstat'), // Kannada
+__('l-kn-in','wp-slimstat'), // Kannada (India)
+__('l-ko','wp-slimstat'), // Korean
+__('l-ko-kr','wp-slimstat'), // Korean (Korea)
+__('l-kok','wp-slimstat'), // Konkani
+__('l-kok-in','wp-slimstat'), // Konkani (India)
+__('l-ky','wp-slimstat'), // Kyrgyz
+__('l-ky-kg','wp-slimstat'), // Kyrgyz (Kyrgyzstan)
+__('l-lt','wp-slimstat'), // Lithuanian
+__('l-lt-lt','wp-slimstat'), // Lithuanian (Lithuania)
+__('l-lv','wp-slimstat'), // Latvian
+__('l-lv-lv','wp-slimstat'), // Latvian (Latvia)
+__('l-mi','wp-slimstat'), // Maori
+__('l-mi-nz','wp-slimstat'), // Maori (New Zealand)
+__('l-mk','wp-slimstat'), // FYRO Macedonian
+__('l-mk-ml','wp-slimstat'), // FYRO Macedonian (Former Yugoslav Republic of Macedonia)
+__('l-mn','wp-slimstat'), // Mongolian
+__('l-mn-mn','wp-slimstat'), // Mongolian (Mongolia)
+__('l-mr','wp-slimstat'), // Marathi
+__('l-mr-in','wp-slimstat'), // Marathi (India)
+__('l-ms','wp-slimstat'), // Malay
+__('l-ms-bn','wp-slimstat'), // Malay (Brunei Darussalam)
+__('l-ms-my','wp-slimstat'), // Malay (Malaysia)
+__('l-mt','wp-slimstat'), // Maltese
+__('l-mt-mt','wp-slimstat'), // Maltese (Malta)
+__('l-nb','wp-slimstat'), // Norwegian (Bokm?l)
+__('l-nb-no','wp-slimstat'), // Norwegian (Bokm?l) (Norway)
+__('l-nl','wp-slimstat'), // Dutch
+__('l-nl-be','wp-slimstat'), // Dutch (Belgium)
+__('l-nl-nl','wp-slimstat'), // Dutch (Netherlands)
+__('l-nn-no','wp-slimstat'), // Norwegian (Nynorsk) (Norway)
+__('l-ns','wp-slimstat'), // Northern Sotho
+__('l-ns-za','wp-slimstat'), // Northern Sotho (South Africa)
+__('l-pa','wp-slimstat'), // Punjabi
+__('l-pa-in','wp-slimstat'), // Punjabi (India)
+__('l-pl','wp-slimstat'), // Polish
+__('l-pl-pl','wp-slimstat'), // Polish (Poland)
+__('l-ps','wp-slimstat'), // Pashto
+__('l-ps-ar','wp-slimstat'), // Pashto (Afghanistan)
+__('l-pt','wp-slimstat'), // Portuguese
+__('l-pt-br','wp-slimstat'), // Portuguese (Brazil)
+__('l-pt-pt','wp-slimstat'), // Portuguese (Portugal)
+__('l-qu','wp-slimstat'), // Quechua
+__('l-qu-bo','wp-slimstat'), // Quechua (Bolivia)
+__('l-qu-ec','wp-slimstat'), // Quechua (Ecuador)
+__('l-qu-pe','wp-slimstat'), // Quechua (Peru)
+__('l-ro','wp-slimstat'), // Romanian
+__('l-ro-ro','wp-slimstat'), // Romanian (Romania)
+__('l-ru','wp-slimstat'), // Russian
+__('l-ru-ru','wp-slimstat'), // Russian (Russia)
+__('l-sa','wp-slimstat'), // Sanskrit
+__('l-sa-in','wp-slimstat'), // Sanskrit (India)
+__('l-se','wp-slimstat'), // Sami (Northern)
+__('l-se-fi','wp-slimstat'), // Sami (Northern) (Finland)
+__('l-se-no','wp-slimstat'), // Sami (Northern) (Norway)
+__('l-se-se','wp-slimstat'), // Sami (Northern) (Sweden)
+__('l-sk','wp-slimstat'), // Slovak
+__('l-sk-sk','wp-slimstat'), // Slovak (Slovakia)
+__('l-sl','wp-slimstat'), // Slovenian
+__('l-sl-si','wp-slimstat'), // Slovenian (Slovenia)
+__('l-sq','wp-slimstat'), // Albanian
+__('l-sq-al','wp-slimstat'), // Albanian (Albania)
+__('l-sr-ba','wp-slimstat'), // Serbian (Latin) (Bosnia and Herzegovina)
+__('l-sr-sp','wp-slimstat'), // Serbian (Latin) (Serbia and Montenegro)
+__('l-sv','wp-slimstat'), // Swedish
+__('l-sv-fi','wp-slimstat'), // Swedish (Finland)
+__('l-sv-se','wp-slimstat'), // Swedish (Sweden)
+__('l-sw','wp-slimstat'), // Swahili
+__('l-sw-ke','wp-slimstat'), // Swahili (Kenya)
+__('l-ta','wp-slimstat'), // Tamil
+__('l-ta-in','wp-slimstat'), // Tamil (India)
+__('l-te','wp-slimstat'), // Telugu
+__('l-te-in','wp-slimstat'), // Telugu (India)
+__('l-th','wp-slimstat'), // Thai
+__('l-th-th','wp-slimstat'), // Thai (Thailand)
+__('l-tl','wp-slimstat'), // Tagalog
+__('l-tl-ph','wp-slimstat'), // Tagalog (Philippines)
+__('l-tn','wp-slimstat'), // Tswana
+__('l-tn-za','wp-slimstat'), // Tswana (South Africa)
+__('l-tr','wp-slimstat'), // Turkish
+__('l-tr-tr','wp-slimstat'), // Turkish (Turkey)
+__('l-tt','wp-slimstat'), // Tatar
+__('l-tt-ru','wp-slimstat'), // Tatar (Russia)
+__('l-ts','wp-slimstat'), // Tsonga
+__('l-uk','wp-slimstat'), // Ukrainian
+__('l-uk-ua','wp-slimstat'), // Ukrainian (Ukraine)
+__('l-ur','wp-slimstat'), // Urdu
+__('l-ur-pk','wp-slimstat'), // Urdu (Islamic Republic of Pakistan)
+__('l-uz','wp-slimstat'), // Uzbek (Latin)
+__('l-uz-uz','wp-slimstat'), // Uzbek (Cyrillic) (Uzbekistan)
+__('l-vi','wp-slimstat'), // Vietnamese
+__('l-vi-vn','wp-slimstat'), // Vietnamese (Viet Nam)
+__('l-xh','wp-slimstat'), // Xhosa
+__('l-xh-za','wp-slimstat'), // Xhosa (South Africa)
+__('l-zh','wp-slimstat'), // Chinese
+__('l-zh-cn','wp-slimstat'), // Chinese (S)
+__('l-zh-hk','wp-slimstat'), // Chinese (Hong Kong)
+__('l-zh-mo','wp-slimstat'), // Chinese (Macau)
+__('l-zh-sg','wp-slimstat'), // Chinese (Singapore)
+__('l-zh-tw','wp-slimstat'), // Chinese (T)
+__('l-zu','wp-slimstat'), // Zulu
+__('l-zu-za','wp-slimstat'), // Zulu (South Africa)
+__('l-','wp-slimstat'), // Unknown
+__('l-empty','wp-slimstat'), // Unknown
+__('l-xx','wp-slimstat'), // Unknown
+
+__('c-xy','wp-slimstat') // Local IP Address
+);
