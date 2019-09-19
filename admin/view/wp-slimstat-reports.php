bf8096b8cf8ecd1627def7a1ee8e2512837114ca
Jason Crouse <support@wp-slimstat.com>
2019-09-19 12:38:57 -0400

    Reverted a change to the Top Web Pages report that was now combining URLs with a trailing slash and ones without one into one result. As James pointed out, this is a less accurate measure and hides the fact that people might be accessing the website in different ways.

--- admin/view/wp-slimstat-reports.php
+++ admin/view/wp-slimstat-reports.php
@@ -114,9 +114,7 @@ class wp_slimstat_reports {
 				'callback' => array( __CLASS__, 'raw_results_to_html' ),
 				'callback_args' => array(
 					'type' => 'top',
-					'columns' => 'TRIM( TRAILING "/" FROM SUBSTRING_INDEX( resource, "' . ( !get_option( 'permalink_structure' ) ? '&' : '?' ) . '", 1 ) )',
-					'as_column' => 'resource',
-					'filter_op' => 'contains',
+					'columns' => 'resource',
 					'raw' => array( 'wp_slimstat_db', 'get_top' )
 				),
 				'classes' => array( 'normal' ),
