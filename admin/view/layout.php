c2d1a9d69260a53a74e21bcc2310476f28785729
ighorbani <imanghorbani.ir@gmail.com>
2023-08-01 11:41:01 +0330

    Fix Customize Page Messy Layout

--- admin/view/layout.php
+++ admin/view/layout.php
@@ -68,7 +68,7 @@
 		echo "
 			<div class='postbox$placeholder_classes' id='".esc_attr($a_report_id)."'>
 				<div class='slimstat-header-buttons'>
-					<a class='slimstat-font-$icon' href='#' title='".esc_attr($title)."></a>
+					<a class='slimstat-font-$icon' href='#' title='".esc_attr($title)."''></a>
 					" . ( ( $a_location_id != 'inactive' ) ? ' <a class="slimstat-font-minus-circled" href="#" title="' . __( 'Move to Inactive', 'wp-slimstat' ) . '"></a>' : '' ) . "
 				</div>
 				<h3 class='hndle'>" . wp_slimstat_reports::$reports[ $a_report_id ][ 'title' ] . "</h3>
