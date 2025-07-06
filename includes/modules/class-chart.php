93d882e41d46a4cb1fbd6a35e430caedf0e84da0
Farzad <fstarlike@gmail.com>
2025-07-06 13:08:22 +0330

    Fix "Previous Period" tooltip

--- includes/modules/class-chart.php
+++ includes/modules/class-chart.php
@@ -398,7 +398,7 @@ class Chart
             'previous_period'         => __('-- Previous Period', 'wp-slimstat'),
             'days_ago'                => sprintf(__('%s Days ago', 'wp-slimstat'), $args['days_between'] ?? 0),
             '30_days_ago'             => __('30 Days ago', 'wp-slimstat'),
-            'previous_period_tooltip' => __("-- Previous Period\nTap here to show/hide comparison.", 'wp-slimstat'),
+            'previous_period_tooltip' => __("Tap “Previous Period” to hide or show the previous period line.", 'wp-slimstat'),
             'today'    => __('Today', 'wp-slimstat'),
             'day_ago'  => __('Day ago', 'wp-slimstat'),
             'year_ago' => __('Year ago', 'wp-slimstat'),
@@ -425,7 +425,7 @@ class Chart
         $this->chart_labels = isset($this->args['chart_labels']) ? $this->args['chart_labels'] : array_keys($this->data['datasets']);
         $this->translations = [
             'previous_period'         => __('-- Previous Period', 'wp-slimstat'),
-            'previous_period_tooltip' => __('Click to Show or Hide data from the previous period for comparison.', 'wp-slimstat'),
+            'previous_period_tooltip' => __('Click Tap “Previous Period” to hide or show the previous period line.', 'wp-slimstat'),
             'today'                   => __('Today', 'wp-slimstat'),
             'days_ago'                => sprintf(__('%s Days ago', 'wp-slimstat'), $this->args['days_between'] ?? 0),
             '30_days_ago'             => __('30 Days ago', 'wp-slimstat'),
