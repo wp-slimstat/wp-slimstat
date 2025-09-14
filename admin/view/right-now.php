e2e095fe8358a3a8e69938cb391dfe4619e4ce48
Farzad Hoseinzade <fstarlike@gmail.com>
2025-09-14 17:35:19 +0330

    Fix IP address display truncation logic in right-now.php to ensure proper ellipsis handling for longer IPs.

--- admin/view/right-now.php
+++ admin/view/right-now.php
@@ -160,7 +160,7 @@ for ($i = 0; $i < $count_page_results; $i++) {
             $ip_address .= sprintf(' %s</a>', $display_user_name);
             $display_ip_value = $results[$i]['ip'];
             if ('on' == (wp_slimstat::$settings['hash_ip'] ?? 'off')) {
-                $display_ip_value = substr($results[$i]['ip'], 0, 12) . '…';
+                $display_ip_value = substr($results[$i]['ip'], 0, 12) . (strlen( $results[$i]['ip'] ) > 12 ? '…' : '');
             } elseif ('on' == wp_slimstat::$settings['anonymize_ip']) {
                 // already masked in storage; still truncate for UI clarity
                 $display_ip_value = $results[$i]['ip'];
